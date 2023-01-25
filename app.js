873a05966a10f2eeebeab830320af10edcead018
invinsibleho <hasan.dev04@gmail.com>
2023-01-26 00:57:30 +0600

    routes added

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
+const tourRoutes = require("./routes/tour.routes");
+
 app.get("/", (req, res) => {
   res.send("Welcome to Tour Management.");
 });
-app.use();
+app.use("api/v1/tours", tourRoutes);
 module.exports = app;
